ee43903a255f0de6a406b4b07ee38b3c31f55be4
Paras0750 <nauriyalparas0@gmail.com>
2023-03-10 18:07:05 +0530

    70% Front End Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,39 @@
 import React from "react";
-import CreateMeal from "./LandingPage/CreateMeal";
-import Footer from "./LandingPage/Footer";
-import Navbar from "./LandingPage/Navbar";
+import Contact from "./Contact";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import WelcomePage from "./WelcomePage";
+import {
+  BrowserRouter,
+  Routes, 
+  Route,
+} from "react-router-dom";
+import Bmi from "./Bmi";
+import Recipe from "./Recipe";
+import Login from "./Login";
+import Register from "./Register";
+import PersonalMeal from "./PersonalMeal";
+// import MouseTracker from "./Test";
 
-const App = () => {
-  return (
-    <div>
+function App() {
+  return (<>
+    {/* <MouseTracker /> */}
+    <BrowserRouter>
       <Navbar />
-      <CreateMeal />
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/analysis" element={<Recipe />} />
+        <Route path="/bmi" element={<Bmi />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/meal" element={<PersonalMeal />} />
+      </Routes>
       <Footer />
-    </div>
+    </BrowserRouter>
+    </>
   );
-};
+}
+
 
 export default App;
---
+++ src/components/slides/ThirdSlide.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Container, TextField } from "@material-ui/core";
+import useStyles from "../style";
+import { Autocomplete } from "@mui/material";
+
+const ThirdSlide = (props) => {
+  const classes = useStyles();
+
+
+  return (
+    <Container className={classes.container}>
+      <Autocomplete
+        disablePortal
+        id="combo-box-demo"
+        options={props.diets}
+        renderInput={(params) => (
+          <TextField {...params} label="Activity Level" />
+        )}
+        maxWidth
+      />
+      <TextField
+        label="How Fast"
+        style={{ display: "block", margin: "12px" }}
+      />
+      <TextField
+        label="Goal Weight"
+        placeholder="Optional"
+        style={{ display: "block", margin: "12px" }}
+      />
+      <TextField
+        label="Goal Body Fat"
+        placeholder="Optional"
+        style={{ display: "block", margin: "12px" }}
+      />
+      <TextField
+        label="Diet Type"
+        placeholder="Optional"
+        style={{ display: "block", margin: "12px" }}
+      />
+    </Container>
+  );
+};
+
+export default ThirdSlide;
